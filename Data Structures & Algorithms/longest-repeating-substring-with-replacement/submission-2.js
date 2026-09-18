f18b84db5097910e6169f9b4a140ab90441df087
Kai <100989619+kaienkhoo@users.noreply.github.com>
2026-09-18 13:12:32 +0800

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let left = 0;
+        let count = {}
+        let maxLength = 0;
+        let maxFreq = 0;
+        
+        for(let right = 0; right < s.length; right++){
+
+            count[s[right]] = (count[s[right]] || 0) + 1;
+
+            maxFreq = Math.max(maxFreq, count[s[right]]);
+
+            while((right - left + 1) - maxFreq > k){
+                count[s[left]]--;
+                left++;
+            }
+
+            maxLength = Math.max(maxLength,right - left + 1);
+        }
+
+        return maxLength;
+
+    }
+}
